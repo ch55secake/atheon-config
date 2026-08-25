556428739f7cb79c056acc09f0421aeb47179a57
ch55secake <oscardjbackup@gmail.com>
2026-08-25 22:43:55 +0100

    chore: add aliases and status

--- configuration.nix
+++ configuration.nix
@@ -1,15 +1,13 @@
 { pkgs, ... }:
 
 {
-  # Determinate manages the Nix installation/daemon.
-  # Prevent nix-darwin from trying to manage it too.	  
   nix.enable = false;
 
   nixpkgs.hostPlatform = "aarch64-darwin";
 
   users.users.oscar = {
-   name = "oscar";
-   home = "/Users/oscar";
+    name = "oscar";
+    home = "/Users/oscar";
   };
 
   system.primaryUser = "oscar";
@@ -24,9 +22,24 @@
   ];
 
   programs.zsh.enable = true;
-
-  # Lets nix-darwin manage itself.
   programs.zsh.enableCompletion = true;
-  
+
+  homebrew = {
+    enable = true;
+    enableZshIntegration = true;
+
+    brews = [
+    ];
+
+    casks = [
+    ];
+
+    onActivation = {
+      autoUpdate = true;
+      upgrade = true;
+      cleanup = "uninstall";
+    };
+  };
+
   system.stateVersion = 6;
 }
--- home.nix
+++ home.nix
@@ -20,12 +20,36 @@
   };
 
   programs.zsh = {
-    enable = true;
+   enable = true;
 
-    shellAliases = {
-      rebuild = "sudo nix run github:nix-darwin/nix-darwin#darwin-rebuild -- switch --flake ~/Projects/atheon-config";
-    };
-  };
+   shellAliases = {
+     rebuild = "sudo nix run github:nix-darwin/nix-darwin#darwin-rebuild -- switch --flake ~/Projects/atheon-config";
+
+     # Navigation
+     ".." = "cd ..";
+     "..." = "cd ../..";
+
+     # Git
+     gst = "git status";
+     ga = "git add";
+     gc = "git commit";
+     gp = "git push";
+     gl = "git pull";
+     gd = "git diff";
+     gco = "git checkout";
+     gb = "git branch";
+    
+     nr = "nix run";
+     update = "cd ~/Projects/atheon-config && nix flake update && rebuild";
+
+     # Better defaults
+     grep = "grep --color=auto";
+     ll = "ls -lah";
+
+     # Nix config
+     nixconfig = "cd ~/Projects/atheon-config";
+   };
+ }; 
 
   programs.home-manager.enable = true;
 }
